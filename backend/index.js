1ebb698b0e1064a43c896b019dc77ecd65315af5
hoangnv134 <hoangnv134@fpt.com>
2026-07-13 12:15:05 +0700

    update

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(cors({
   origin: [
     'http://localhost:5500', 'http://127.0.0.1:5500',
     'http://localhost:8080', 'http://127.0.0.1:8080',
-    'http://localhost:5173', 'http://127.0.0.1:5173',
+    'http://localhost:5173', 'http://127.0.0.1:5173','https://toan-lop-3-8e8h.vercel.app', 
   ],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
